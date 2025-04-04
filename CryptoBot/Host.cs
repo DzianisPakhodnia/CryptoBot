306fd88020bed72c19b10c7ec71623595843785a
Denis Pokhodnia <denispokhodnia@gmail.com>
2025-04-05 02:24:55 +0300

    Добавьте файлы проекта.

---
+++ CryptoBot/Host.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace CryptoBot
+{
+    public class Host
+    {
+        public Action<ITelegramBotClient, Update>? onMessage;
+
+        private TelegramBotClient _bot;
+
+
+
+        public Host(string token)
+        {
+            _bot = new TelegramBotClient(token);
+
+        }
+
+        public void Start()
+        {
+            _bot.StartReceiving(UpdateHandler, ErrorHandler);
+        }
+
+        private async Task ErrorHandler(ITelegramBotClient client, Exception exception, HandleErrorSource source, CancellationToken token)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        private async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            var chatId = message.Chat.Id;
+
+            if (message.Text != null)
+            {
+
+                Console.WriteLine($"{message.Chat.Username ?? "Анон"} | {message.Text}");
+                try
+                {
+                    if (message.Text.ToLower().Contains("привет"))
+                    {
+                        await _bot.SendMessage(chatId, "Ку и тебе");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка при отправке сообщения: {ex.Message}");
+                }
+
+                return;
+            }
+
+
+            if (message.Photo != null)
+            {
+                await _bot.SendMessage(chatId, "Норм фото но скинь файлом");
+                return;
+            }
+
+            try
+            {
+                if (message.Text.ToLower().Contains("привет"))
+                {
+                    await _bot.SendMessage(chatId, "Ку и тебе");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при отправке сообщения: {ex.Message}");
+            }
+        }
+
+    }
+
+
+
+
+
+}
---
+++ CryptoBot/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+namespace CryptoBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var config = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+
+            string? apiKey = config["TelegramBot:ApiKey"];
+
+            var client = new Host(apiKey);
+
+            client.Start();
+
+            Console.ReadLine();
+        }
+
+
+
+
+
+
+
+    }
+}
